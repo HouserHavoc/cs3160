2c5233bf37b7caee5a1657ea4047d7d51026190c
saasbook <saasbook@saasbook.(none)>
2014-10-02 09:18:42 -0300

    First commit

---
+++ potaoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potaoes::Application.config.secret_token = 'a270ce586c50995fcf56cfdb01eb87745bdc87d49630f4168f6fe1a6f35c410c6298445aa447935216c135775e0e545e6589acccb609d8f4afef8e4798de6cf8'
